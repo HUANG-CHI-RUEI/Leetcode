3cf872ddfc0be9de481e9ec940f9ec1853801430
HUANG-CHI-RUEI <pei4b066@gmail.com>
2021-10-02 14:02:39 +0800

    leetcode 286

---
+++ Leetcode/leetcode 286.py
@@ -0,0 +1,44 @@
+# https://www.cnblogs.com/grandyang/p/5285868.html
+# You are given a m x n 2D grid initialized with these three possible values.
+
+# -1 - A wall or an obstacle.
+# 0 - A gate.
+# INF - Infinity means an empty room. We use the value 2 31 - 1 = 2147483647 to represent INF as you may assume that the distance to a gate is less than 2147483647.
+# Fill each empty room with the distance to its nearest gate. If it is impossible to reach a gate, it should be filled with INF.
+'''
+Example:
+Given the 2D grid:
+INF -1 0 INF
+INF INF INF -1
+INF -1 INF -1
+0 -1 INF INF
+After running your function, the 2D grid should be:
+3 -1 0 1
+2 2 1 -1
+1 -1 2 -1
+0 -1 3 4
+'''
+# 1.dfs: 
+# TC:O(m*n*4)
+# SC:O(m*n*4)
+
+class Solution:
+    def walsAndGates(self, room):
+        if not rooms:
+            return []
+        m, n = len(room), len(room[0])
+        
+        def dfs(x, y, dis):
+            if x < 0 or x >= m or y < 0 or y >= n or room[x][y] < dis:
+                return 
+            room[x][y] = dis
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                dfs(i+dx, j+dy, dis+1)
+
+        for x in range(m):
+            for y in range(n):
+                if room[x][y] == '0':
+                    dfs(x, y, 0)
+
+        
+        
